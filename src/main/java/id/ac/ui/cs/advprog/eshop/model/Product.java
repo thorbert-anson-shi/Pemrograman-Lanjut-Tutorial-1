a27bd122612c7cdf314e5958937d6e5c9214fb42
thorbert <tobyas2005139@gmail.com>
2025-02-12 12:52:30 +0700

    Added validation using Hibernate

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,10 +1,20 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+
+@Getter @Setter
 public class Product {
+    @NotNull
     private String productId;
+
+    @NotBlank(message="Product name cannot be empty")
     private String productName;
+
+    @Min(value=0, message="Product quantity cannot go lower than 0")
     private int productQuantity;
 }
